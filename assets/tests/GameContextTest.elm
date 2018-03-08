6aecfe28eac382c7bcd482de06acacac3bbd4278
Zack.Kayser <zack.kayser@kroger.com>
2018-03-07 21:30:10 -0500

    Add fakeTile helper function

--- assets/tests/GameContextTest.elm
+++ assets/tests/GameContextTest.elm
@@ -45,3 +45,8 @@ initialTiles : List Tile
 initialTiles =
     List.map (\( number, letter ) -> { letter = letter, id = number, value = number })
         [ ( 1, "A" ), ( 2, "B" ), ( 3, "C" ), ( 4, "D" ), ( 5, "E" ), ( 6, "F" ), ( 7, "G" ) ]
+
+
+fakeTileWith : Int -> String -> Tile
+fakeTileWith number letter =
+    { letter = letter, id = number, value = number }
