2d68a66a53dab520f61c68e657652b902552113e
Kayser <zkayser@gmail.com>
2018-04-08 15:05:57 -0400

    Move dictionary-related files into their own directories

--- lib/elm_scrabble/dictionary/words_api_exception.ex
+++ lib/elm_scrabble/dictionary/words_api_exception.ex
@@ -1,11 +1,11 @@
 defmodule WordsApiException do
-	@message "Must have `WORDS_API_KEY` environment variable set to use the WordsApi module.\n
+  @message "Must have `WORDS_API_KEY` environment variable set to use the WordsApi module.\n
 						If you do not want to sign up for a Rapid API account, go into your dev.exs config
 						file and change the value for :dictionary_api to use FakeDictionaryApi instead of
 						WordsApi."
-	defexception [:message]
+  defexception [:message]
 
-	def exception(value) do
-		%WordsApiException{message: @message}
-	end
+  def exception(_value) do
+    %WordsApiException{message: @message}
+  end
 end
