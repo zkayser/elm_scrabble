1dbf9ac5e595c84daa0f4b011f05308b3b67772c
Kayser <zkayser@gmail.com>
2018-04-11 21:58:13 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElmScrabble.Mixfile do
   def project do
     [
       app: :elm_scrabble,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
