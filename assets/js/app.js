407b95e13d233aeaca78e21e8204880631f207f9
Kayser <zkayser@gmail.com>
2019-01-03 07:36:05 -0500

    Remove leftover console.log statement

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,4 @@ import { Elm } from '../elm/Main.elm';
 import css from '../css/app.css';
 
 const ELM_DIV = document.getElementById("elm-scrabble");
-
-console.log('Here is Elm.Main: ', Elm.Main);
 let elmApp = Elm.Main.init(ELM_DIV);
