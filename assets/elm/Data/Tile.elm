08e36ce1f80b06b59045b21af98ce66c9f9eb2e8
Kayser <zkayser@gmail.com>
2019-02-14 08:59:05 -0500

    Add drag disabled Tile markup to Tile module

--- assets/elm/Data/Tile.elm
+++ assets/elm/Data/Tile.elm
@@ -1,9 +1,10 @@
-module Data.Tile exposing (Tile, toHtml)
+module Data.Tile exposing (Tile, disable, toHtml)
 
 import Data.Multiplier exposing (Multiplier(..))
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Widgets.DragAndDrop exposing (Config, draggable)
+
 
 type alias Tile =
     { letter : String
@@ -15,11 +16,19 @@ type alias Tile =
 
 toHtml : Config msg Tile cell -> Tile -> Html msg
 toHtml config tile =
-  let
-      dragConfig =
-          draggable (config.dragStartMsg tile) config.dragEndMsg
-  in
-  div ([ class "cell tile" ] ++ dragConfig)
-      [ span [ class "letter" ] [ text tile.letter ]
-      , span [ class "value" ] [ text <| String.fromInt tile.value ]
-      ]
+    let
+        dragConfig =
+            draggable (config.dragStartMsg tile) config.dragEndMsg
+    in
+    div ([ class "cell tile" ] ++ dragConfig)
+        [ span [ class "letter" ] [ text tile.letter ]
+        , span [ class "value" ] [ text <| String.fromInt tile.value ]
+        ]
+
+
+disable : Tile -> Html msg
+disable tile =
+    div [ class "cell tile" ]
+        [ span [ class "letter" ] [ text tile.letter ]
+        , span [ class "value" ] [ text <| String.fromInt tile.value ]
+        ]
--- assets/elm/tests/Data/TileTest.elm
+++ assets/elm/tests/Data/TileTest.elm
@@ -1,48 +1,66 @@
 module Data.TileTest exposing (suite)
 
 import Data.Cell exposing (Cell)
-import Data.Tile as Tile exposing (Tile)
 import Data.Multiplier exposing (Multiplier(..))
+import Data.Tile as Tile exposing (Tile)
 import Expect
+import Html.Attributes as Attribute
 import Test exposing (..)
 import Test.Html.Query as Query
-import Test.Html.Selector exposing (text, classes)
+import Test.Html.Selector exposing (attribute, classes, text)
 
 
 suite : Test
 suite =
     describe "Tile" <|
-      let
-        dragNDropConfig =
-          { dragStartMsg = DragStarted
-          , dragEndMsg = DragEnd
-          , dropMsg = Dropped
-          , dragOverMsg = DragOver
-          }
-        tile =
-          { letter = "A"
-          , id = 1
-          , value = 4
-          , multiplier = NoMultiplier
-          }
-      in
+        let
+            dragNDropConfig =
+                { dragStartMsg = DragStarted
+                , dragEndMsg = DragEnd
+                , dropMsg = Dropped
+                , dragOverMsg = DragOver
+                }
+
+            tile =
+                { letter = "A"
+                , id = 1
+                , value = 4
+                , multiplier = NoMultiplier
+                }
+        in
         [ describe "toHtml"
-          [ test "renders the tile's letter" <|
-            \_ ->
-              Tile.toHtml dragNDropConfig tile
-              |> Query.fromHtml
-              |> Query.has
-                [ text tile.letter
-                , text <| String.fromInt tile.value
-                , classes [ "cell", "tile" ]
-                , classes [ "letter" ]
-                , classes [ "value" ]
-                ]
-          ]
+            [ test "renders the tile's letter" <|
+                \_ ->
+                    Tile.toHtml dragNDropConfig tile
+                        |> Query.fromHtml
+                        |> Query.has
+                            [ text tile.letter
+                            , text <| String.fromInt tile.value
+                            , classes [ "cell", "tile" ]
+                            , classes [ "letter" ]
+                            , classes [ "value" ]
+                            , attribute <| Attribute.attribute "draggable" "true"
+                            ]
+            , test "does not add draggable attribute when the tile is not draggable" <|
+                \_ ->
+                    Tile.disable tile
+                        |> Query.fromHtml
+                        |> Expect.all
+                            [ Query.hasNot [ attribute <| Attribute.attribute "draggable" "true" ]
+                            , Query.has
+                                [ text tile.letter
+                                , text <| String.fromInt tile.value
+                                , classes [ "cell", "tile" ]
+                                , classes [ "letter" ]
+                                , classes [ "value" ]
+                                ]
+                            ]
+            ]
         ]
 
+
 type TestMsg
-  = DragStarted Tile
-  | DragEnd
-  | Dropped Cell
-  | DragOver Cell
+    = DragStarted Tile
+    | DragEnd
+    | Dropped Cell
+    | DragOver Cell
