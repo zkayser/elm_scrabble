84333d8be12c6af4568ff264468c3116f65be6d8
Kayser <zkayser@gmail.com>
2019-02-21 15:14:30 -0500

    Add fuzz tests for position decoding

--- assets/elm/tests/Data/PositionTest.elm
+++ assets/elm/tests/Data/PositionTest.elm
@@ -2,8 +2,11 @@ module Data.PositionTest exposing (suite)
 
 import Data.Position as Position
 import Expect
-import Json.Decode exposing (decodeValue)
+import Fuzz
+import Fuzzers.Position as PositionFuzzer
+import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
+import Random
 import Result
 import Test exposing (..)
 
@@ -22,4 +25,27 @@ suite =
                 in
                 decodeValue Position.decode encodedPosition
                     |> Expect.equal (Result.Ok ( 8, 8 ))
+        , fuzz2 (Fuzz.intRange 0 Random.maxInt) (Fuzz.intRange 0 Random.maxInt) "decoding is reversible" <|
+            \col row ->
+                let
+                    encoded =
+                        Encode.object
+                            [ ( "col", Encode.int col )
+                            , ( "row", Encode.int row )
+                            ]
+                    decode = Decode.int
+                in
+                decodeValue Position.decode encoded
+                    |> Expect.all
+                        [ ( \position ->
+                            case position of
+                                Ok pos -> Expect.equal (Tuple.second pos) row
+                                _ -> Expect.fail "Expected row to be decoded"
+                          )
+                        , ( \position ->
+                            case position of
+                                Ok pos -> Expect.equal (Tuple.first pos) col
+                                _ -> Expect.fail "Expected col to be decoded"
+                           )
+                        ]
         ]
--- assets/elm/tests/Fuzzers/Position.elm
+++ assets/elm/tests/Fuzzers/Position.elm
@@ -1,10 +1,32 @@
-module Fuzzers.Position exposing (fuzzer)
+module Fuzzers.Position exposing (fuzzer, encodeFuzzer)
 
 import Data.Position exposing (Position)
 import Fuzz exposing (..)
+import Json.Encode as Encode exposing (Value)
 import Random
 
 
 fuzzer : Fuzzer Position
 fuzzer =
     Fuzz.tuple ( Fuzz.int, Fuzz.int )
+
+encodeFuzzer : Fuzzer Value
+encodeFuzzer =
+  Fuzz.map2
+    (\col row ->
+      Encode.object
+        [ col
+        , row
+        ]
+    )
+  rowEncoder
+  colEncoder
+
+
+rowEncoder : Fuzzer ( String, Value )
+rowEncoder =
+  Fuzz.tuple ( Fuzz.constant "row", Fuzz.map Encode.int <| Fuzz.intRange 0 Random.maxInt )
+
+colEncoder : Fuzzer ( String, Value )
+colEncoder =
+  Fuzz.tuple ( Fuzz.constant "col", Fuzz.map Encode.int <| Fuzz.intRange 0 Random.maxInt )
